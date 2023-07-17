fa5a1f2fcb24e425e73dad4e9166a90ab7bf2722
Ilya Gerasimov <py1lya@yandex.ru>
2023-07-17 23:23:26 +0300

    git push

---
+++ Move Zeroes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums: int) -> None:
+        self.nums = nums
+
+        for i in range(len(self.nums)):
+            if self.nums[i] == 0:
+                self.nums.append(self.nums[i])
+                self.nums.remove(0)
+        
+        return self.nums
+#Yfx
