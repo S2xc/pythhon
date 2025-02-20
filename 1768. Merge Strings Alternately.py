6a2ea1db8f0e291707e6487e26b33013d9970214
S2xc <py1lya@yandex.ru>
2025-02-20 18:51:45 +0300

    chore: update dependencies

--- 1768. Merge Strings Alternately.py
+++ 1768. Merge Strings Alternately.py
@@ -2,6 +2,8 @@ class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         l1, l2 = len(word1), len(word2)
         max = l1 if l1 > l2 else l2
+
+# Refactored function for better performance - 2025-02-20 18:51:45
         res = ""
         for i in range(max):
             if l1 > i:
