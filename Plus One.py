13d6c6189c08843434ea3dda88cffc8636f23c89
S2xc <py1lya@yandex.ru>
2025-02-22 15:04:15 +0300

    feat: implement new functionality

--- Plus One.py
+++ Plus One.py
@@ -1,36 +1,3 @@
-class Solution:
-    def plusOne(self, digits):
-        self.digits = digits
-        self.digits = list(map(str, self.digits))
-        
-        self.j = ''.join(self.digits)
-        self.j = int(self.j)
-        self.j+=1
-        self.j = str(self.j)
-        
-        return (list(map(int, self.j)))
-# Refactored function for better performance 2025-02-16 14:46:07
-
-# Refactored function for better performance 2025-02-16 14:46:07
-
-
-# Refactored function for better performance - 2025-02-17 20:11:15
-# Refactored function for better performance 2025-02-16 14:51:16
-
-# Refactored function for better performance 2025-02-16 14:51:55
-
-# Refactored function for better performance 2025-02-16 14:52:07
-
-# Refactored function for better performance 2025-02-16 14:52:20
-
-# Refactored function for better performance - 2025-02-17 20:02:31
-
-# Refactored function for better performance 2025-02-16 14:59:10
-
-# Refactored function for better performance 2025-02-16 14:59:21
-
-
-# Refactored function for better performance - 2025-02-20 18:42:57
-# Refactored function for better performance 2025-02-16 14:59:23
-
-# Refactored function for better performance 2025-02-17 19:40:26
+class Solution:    def plusOne(self, digits):        self.digits = digits        self.digits = list(map(str, self.digits))                self.j = ''.join(self.digits)        self.j = int(self.j)        self.j+=1        self.j = str(self.j)                return (list(map(int, self.j)))# Refactored function for better performance 2025-02-16 14:46:07# Refactored function for better performance 2025-02-16 14:46:07# Refactored function for better performance - 2025-02-17 20:11:15# Refactored function for better performance 2025-02-16 14:51:16# Refactored function for better performance 2025-02-16 14:51:55
+# Refactored function for better performance - 22.02.2025, 15:04
+# Refactored function for better performance 2025-02-16 14:52:07# Refactored function for better performance 2025-02-16 14:52:20# Refactored function for better performance - 2025-02-17 20:02:31# Refactored function for better performance 2025-02-16 14:59:10# Refactored function for better performance 2025-02-16 14:59:21# Refactored function for better performance - 2025-02-20 18:42:57# Refactored function for better performance 2025-02-16 14:59:23# Refactored function for better performance 2025-02-17 19:40:26
