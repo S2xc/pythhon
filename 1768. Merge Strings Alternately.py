77a8bd008dc3a07e607161fd49ce1c6f3a5daa89
S2xc <py1lya@yandex.ru>
2025-02-22 15:04:20 +0300

    refactor: optimize performance

--- 1768. Merge Strings Alternately.py
+++ 1768. Merge Strings Alternately.py
@@ -1,31 +1,3 @@
-class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        l1, l2 = len(word1), len(word2)
-        max = l1 if l1 > l2 else l2
-
-# Refactored function for better performance - 2025-02-20 18:51:45
-        res = ""
-        for i in range(max):
-            if l1 > i:
-                res += word1[i]
-            if l2 > i:
-                res += word2[i]
-
-# Refactored function for better performance - 2025-02-20 18:51:47
-        
-        return res
-# Refactored function for better performance 2025-02-16 14:46:07
-
-# Refactored function for better performance 2025-02-16 14:46:07
-
-
-# Refactored function for better performance - 2025-02-17 20:02:29
-# Refactored function for better performance 2025-02-16 14:46:07
-
-# Refactored function for better performance 2025-02-16 14:51:52
-
-# Refactored function for better performance 2025-02-16 14:52:20
-
-# Refactored function for better performance 2025-02-16 15:11:31
-
-# Refactored function for better performance 2025-02-16 15:11:35
+class Solution:    def mergeAlternately(self, word1: str, word2: str) -> str:        l1, l2 = len(word1), len(word2)        max = l1 if l1 > l2 else l2# Refactored function for better performance - 2025-02-20 18:51:45        res = ""        for i in range(max):            if l1 > i:                res += word1[i]            if l2 > i:                res += word2[i]# Refactored function for better performance - 2025-02-20 18:51:47                return res# Refactored function for better performance 2025-02-16 14:46:07# Refactored function for better performance 2025-02-16 14:46:07
+# Refactored function for better performance - 22.02.2025, 15:04
+# Refactored function for better performance - 2025-02-17 20:02:29# Refactored function for better performance 2025-02-16 14:46:07# Refactored function for better performance 2025-02-16 14:51:52# Refactored function for better performance 2025-02-16 14:52:20# Refactored function for better performance 2025-02-16 15:11:31# Refactored function for better performance 2025-02-16 15:11:35
