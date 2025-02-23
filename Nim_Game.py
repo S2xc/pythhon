645b20cd4733f397c0baf5d3a694a3bdbff11619
S2xc <py1lya@yandex.ru>
2025-02-23 10:48:04 +0300

    refactor: optimize performance

--- Nim_Game.py
+++ Nim_Game.py
@@ -1,18 +1,3 @@
-class Solution:
-    def canWinNim(self, n: int) -> bool:
-        return not (n % 4 == 0)
-# Refactored function for better performance 2025-02-16 14:46:07
-
-# Refactored function for better performance - 2025-02-17 20:11:18
-
-# Refactored function for better performance - 2025-02-17 20:11:12
-
-
-# Refactored function for better performance - 2025-02-20 18:51:52
-# Refactored function for better performance 2025-02-16 14:51:42
-
-# Refactored function for better performance 2025-02-16 14:51:52
-
-# Refactored function for better performance 2025-02-16 14:59:32
-
-# Refactored function for better performance 2025-02-16 16:43:44
+class Solution:    def canWinNim(self, n: int) -> bool:        return not (n % 4 == 0)# Refactored function for better performance 2025-02-16 14:46:07# Refactored function for better performance - 2025-02-17 20:11:18# Refactored function for better performance - 2025-02-17 20:11:12# Refactored function for better performance - 2025-02-20 18:51:52
+# Refactored function for better performance - 23.02.2025, 10:48
+# Refactored function for better performance 2025-02-16 14:51:42# Refactored function for better performance 2025-02-16 14:51:52# Refactored function for better performance 2025-02-16 14:59:32# Refactored function for better performance 2025-02-16 16:43:44
