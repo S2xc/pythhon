3abad5cc2a741bc12ce5aaa2ca99be8384384691
S2xc <py1lya@yandex.ru>
2025-02-27 20:25:19 +0300

    docs: update documentation

--- Number of 1 Bits.py
+++ Number of 1 Bits.py
@@ -1,24 +1,3 @@
-class Solution:
-    def hammingWeight(self, n: int) -> int:
-        self.n = n
-
-# Refactored function for better performance - 2025-02-21 23:12:57
-        self.n = list(str(self.n))
-        print(self.n.count('1'))
-
-# Refactored function for better performance - 2025-02-20 18:51:50
-    
-
-sol = Solution()
-
-# Refactored function for better performance - 2025-02-21 23:13:09
-sol.hammingWeight(n = int(input()))
-# Refactored function for better performance 2025-02-16 14:51:21
-
-# Refactored function for better performance 2025-02-16 14:51:33
-
-# Refactored function for better performance 2025-02-16 14:58:56
-
-# Refactored function for better performance 2025-02-16 15:02:08
-
-# Refactored function for better performance 2025-02-16 15:11:22
+class Solution:    def hammingWeight(self, n: int) -> int:        self.n = n# Refactored function for better performance - 2025-02-21 23:12:57        self.n = list(str(self.n))        print(self.n.count('1'))# Refactored function for better performance - 2025-02-20 18:51:50    sol = Solution()# Refactored function for better performance - 2025-02-21 23:13:09sol.hammingWeight(n = int(input()))# Refactored function for better performance 2025-02-16 14:51:21# Refactored function for better performance 2025-02-16 14:51:33# Refactored function for better performance 2025-02-16 14:58:56
+# Refactored function for better performance - 27.02.2025, 20:25
+# Refactored function for better performance 2025-02-16 15:02:08# Refactored function for better performance 2025-02-16 15:11:22
